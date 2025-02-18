1043b87d9c60ce560a45a4ba5ec01a399fc3752b
Dixita Kikani <Dixita Kikani@LAPTOP-0A03ES1A>
2025-02-19 00:08:56 +0530

    Edit Functionality added

--- SimpleBookCatelog.Infrastructure/Repositories/BookRepository.cs
+++ SimpleBookCatelog.Infrastructure/Repositories/BookRepository.cs
@@ -24,5 +24,17 @@ namespace SimpleBookCatelog.Infrastructure.Repositories
             var books = await context.Books.ToListAsync();
             return books;
         }
+
+        public async Task<Book?> GetByIdAsync(int id)
+        {
+            var book = await context.Books.FirstOrDefaultAsync(e => e.Id == id);
+            return book;
+        }
+
+        public Task UpdateAsync(Book book)
+        {
+            context.Entry(book).State = EntityState.Modified; 
+            return context.SaveChangesAsync();
+        }
     }
 }
