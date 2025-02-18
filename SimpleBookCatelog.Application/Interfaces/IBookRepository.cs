0f4365eb237c5bbd3d5b260f3226a2888f04e70d
Dixita Kikani <Dixita Kikani@LAPTOP-0A03ES1A>
2025-02-19 00:55:42 +0530

    added delete functionality

--- SimpleBookCatelog.Application/Interfaces/IBookRepository.cs
+++ SimpleBookCatelog.Application/Interfaces/IBookRepository.cs
@@ -11,5 +11,7 @@ namespace SimpleBookCatelog.Application.Interfaces
         Task<Book?> GetByIdAsync(int id);
 
         Task UpdateAsync(Book book);
+
+        Task DeleteByIdAsync(int id);
     }
 }
